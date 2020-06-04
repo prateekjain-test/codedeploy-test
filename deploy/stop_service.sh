69f5800487cab8b5327ad5e52812f05f7cb87ac7
prateekjain-test <rajsinha98761@gmail.com>
2020-06-04 11:44:21 +0530

    Create stop_service.sh

---
+++ deploy/stop_service.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Import the common script which will have the generic paramters for configuration
+. ./common.sh
+
+# get the pid file for the service
+PID_FILE=$SERVICE_ROOT/service.pid
+
+if [ -f $PID_FILE ]; then
+    cat $PID_FILE | xargs kill -9
+    rm $PID_FILE
+fi
+
+echo "$SERVICE_NAME stopped successfully..."
