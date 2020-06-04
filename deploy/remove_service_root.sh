47c53c51ad442d5c5a41bb22fde14d39a2852710
prateekjain-test <rajsinha98761@gmail.com>
2020-06-04 13:22:53 +0530

    Update remove_service_root.sh

--- deploy/remove_service_root.sh
+++ deploy/remove_service_root.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+TEST_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+echo $TEST_DIR
+
 # Import the common script which will have the generic paramters for configuration
 source "$DIR/common.sh"
 
