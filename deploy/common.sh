711a0073bf7b0c282f6742681b4f45744c21ff2d
prateekjain-test <rajsinha98761@gmail.com>
2020-06-04 12:31:06 +0530

    Update common.sh

--- deploy/common.sh
+++ deploy/common.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+# Define the root path
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_DIR="$(dirname "$DIR")"
+
 # Import the independent service script which will
 # independent service configuration using which we can
 # run the codedeploy script with dynamic paramters
-. ./properties.sh
+. $(dirname "$0")/properties.sh
 
 # deployment home
 DEPLOYMENT_HOME=/home/ubuntu/codedeploy
