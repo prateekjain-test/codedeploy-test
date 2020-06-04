505797edb879f3590f79907364a162d0a688a710
prateekjain-test <rajsinha98761@gmail.com>
2020-06-04 12:31:54 +0530

    Update remove_service_root.sh

--- deploy/remove_service_root.sh
+++ deploy/remove_service_root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Import the common script which will have the generic paramters for configuration
-. ./common.sh
+. $(dirname "$0")/common.sh
 
 # Remove service root directory
 rm -rf $SERVICE_ROOT
