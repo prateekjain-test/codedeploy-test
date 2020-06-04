1bed53bcf48ab82b33f975b98e31e45e33bdde02
prateekjain-test <rajsinha98761@gmail.com>
2020-06-04 13:39:25 +0530

    Update download_bundle.sh

--- deploy/download_bundle.sh
+++ deploy/download_bundle.sh
@@ -7,10 +7,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source "$DIR/common.sh"
 
 # Define the parent directory where the file will be downloaded from s3 bucket
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-CODEDEPLOY_PARENT_DIR="$(dirname "$DIR")"
+PARENT_DIR="$(dirname "$DIR")"
 
 mkdir -p "$SERVICE_ROOT"
-cp $CODEDEPLOY_PARENT_DIR/target/$SERVICE_NAME-*-SNAPSHOT.jar $SERVICE_ROOT/$SERVICE_NAME-*-SNAPSHOT.jar
+cp "$PARENT_DIR/target/$SERVICE_NAME-*-SNAPSHOT.jar" "$SERVICE_ROOT/$SERVICE_NAME-*-SNAPSHOT.jar"
 
 echo "Successfully copied $SERVICE_NAME jar to $SERVICE_ROOT"
