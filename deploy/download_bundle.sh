d3a8fb8f6864947ecc90bd935d94fb77db2e5b15
prateekjain-test <rajsinha98761@gmail.com>
2020-06-04 13:38:51 +0530

    Update download_bundle.sh

--- deploy/download_bundle.sh
+++ deploy/download_bundle.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Get the directory path
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
 # Import the common script which will have the generic paramters for configuration
 source "$DIR/common.sh"
 
